b4a9e0b20ecb69efb4269e5e095db2a21af6fffe
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2025-06-20 21:46:53 +0600

    Add excerpt customization with read more link and set excerpt length to 40 words

--- inc/default.php
+++ inc/default.php
@@ -12,3 +12,21 @@ add_theme_support('post-thumbnails', [
     'post',
     'page',
 ]);
+
+add_image_size('post-thumbnails', 970, 350, true);
+
+// Excerpt to 40 word
+
+function wpzone_excerpt_more($length)
+{
+    return '<br><br><a href="' . get_permalink(get_the_ID()) . '" class="btn btn-primary btn-sm mt-2 mb-2 mx-4">Read More</a>';
+}
+
+add_filter('excerpt_more', 'wpzone_excerpt_more');
+
+function wpzone_excerpt_length($length)
+{
+    return 40; // Set the excerpt length to 40 words
+}
+
+add_filter('excerpt_length', 'wpzone_excerpt_length');
