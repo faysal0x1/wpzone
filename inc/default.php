4ba193231446b9c8d4ef702b9532c6dbeda6299b
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2025-06-20 21:40:04 +0600

    Enhance default theme setup by adding support for post thumbnails and refining the home page layout with improved post display logic

--- inc/default.php
+++ inc/default.php
@@ -6,3 +6,9 @@
  */
 // Theme Title
 add_theme_support('title-tag');
+
+// Add Thumbnails support
+add_theme_support('post-thumbnails', [
+    'post',
+    'page',
+]);
--- index.php
+++ index.php
@@ -6,48 +6,58 @@
     get_header();
 ?>
 
-<!-- Hero Section Start -->
-<section id="hero">
-    <div class="container-fluid bg-light my-6 mt-0" id="home">
-        <div class="container">
-            <div class="row g-5 align-items-center">
-                <div class="col-lg-6 py-6 pb-0 pt-lg-0">
-                    <h3 class="text-primary mb-3">I'm</h3>
-                    <h1 class="display-3 mb-3">Kate Winslet</h1>
-                    <h2 class="typed-text-output d-inline"></h2>
-                    <div class="typed-text d-none">Web Designer, Web Developer, Front End Developer, Apps Designer,
-                        Apps
-                        Developer</div>
-                    <div class="d-flex align-items-center pt-5">
-                        <a href="" class="btn btn-primary py-3 px-4 me-5">Download CV</a>
-                        <button type="button" class="btn-play" data-bs-toggle="modal"
-                            data-src="https://www.youtube.com/embed/DWRcNpR6Kdc" data-bs-target="#videoModal">
-                            <span></span>
-                        </button>
-                        <h5 class="ms-4 mb-0 d-none d-sm-block">Play Video</h5>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/profile.png" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- Header End -->
 
 <!-- Body Area --->
 
 <section id="body-area">
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-9">
                 <?php
-                    // Display the main content of the page
-                    // while (have_posts()): the_post();
-                    the_content();
-                    // endwhile;
 
+                    // Start the loop to display posts
+                    if (have_posts()):
+                        while (have_posts()): the_post();
+                        ?>
+
+                <div class="card mb-3">
+                    <div class="card-body">
+
+                        <h1 class="card-header">
+                            <?php
+                                                                      if (has_post_thumbnail()) {
+                                                                          the_post_thumbnail('thumbnail', ['class' => 'card-img-top']);
+
+                                                                      }
+
+                                                                  ?>
+                        </h1>
+                        <h5 class="card-title">
+
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        </h5>
+                        <p class="card-text">
+                            <?php the_excerpt(); ?>
+                        </p>
+                    </div>
+                </div>
+
+                <?php
+                                        endwhile;
+                                    else:
+                                        _e('No posts found.', 'wpzone');
+                                    endif;
+
+                                ?>
+            </div>
+            <div class="col-md-3">
+                <?php
+                    // Display the sidebar content
+                    if (is_active_sidebar('home-sidebar')) {
+                        dynamic_sidebar('home-sidebar');
+                    } else {
+                        echo '<h2>This is the sidebar.</h2>';
+                    }
                 ?>
             </div>
         </div>
@@ -58,8 +68,6 @@
 
 
 <!-- Footer Start -->
-
-
 
 <?php get_footer(); ?>
 <!-- Footer End -->
